c81046c342a36583f5c5595edeaf836f6cfce8e1
123 <123>
2024-02-08 18:35:37 +0300

    additionaltask

---
+++ transfer3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
+)
+
+func convertion(arg string, index int) {
+	num := strconv.Itoa(index)
+	num = "output" + num + ".mp3"
+	err := ffmpeg_go.Input(arg).
+		Output(num).
+		OverWriteOutput().Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	filePath := os.Args // define the variable which contains filepath, using command argument
+	for i := 1; i < 4; i++ {
+		go convertion(filePath[i], i)
+	}
+	time.Sleep(time.Second * 3)
+}
+/*
+Тут я решил все через функцию, чтобы не было повторяющегося кода
